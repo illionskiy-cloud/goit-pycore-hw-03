528f771330ac18f6ab2746d8f52b58e97a85ea62
Vladyslav Vasianovych <illionskiy@gmail.com>
2026-02-10 15:57:55 +0200

    перевірка min>max

--- py_hw_4_date.py
+++ py_hw_4_date.py
@@ -7,11 +7,11 @@ def get_days_from_today(date: str) -> int:
     try:
         date = datetime.strptime(date, "%Y-%m-%d")
         if date:
-            delta_days = today.toordinal() - date.toordinal() # Різниця в днях
+            delta_days = (today - date.date()).days # Різниця в днях
             return delta_days
     except ValueError:  # Виключення помилки формату дати
         print("Please provide a date in the format YYYY-MM-DD.")
 
         
-print(get_days_from_today("2024-01-01"))
+print(get_days_from_today("2027-01-01"))
 
--- py_hw_4_getnumbers.py
+++ py_hw_4_getnumbers.py
@@ -12,10 +12,12 @@ except ValueError: # Виключення помилки при введенні
 def get_numbers_ticket(min, max, quantity):
     
         numbers = list(range(min, max + 1))
-        if quantity > len(numbers): # quantity не може бути більше, ніж кількість чисел у діапазоні
-            return print(f'Quantity cannot be greater than {max}')
+        if min >= max: # мінімальне число не може бути більше або рівне максимальному
+            print('Minimum number must be less than maximum number.')
         elif min < 1 or max > 1000: # за умовою - не більше 1000, не менше 1
-            return print('Minimum and maximum nubers must be between 1 and 1000.')
+            print('Minimum and maximum nubers must be between 1 and 1000.')
+        elif quantity > len(numbers): # quantity не може бути більше, ніж кількість чисел у діапазоні
+            print(f'Quantity cannot be greater than {max}')
         else:
             lottery_numbers = random.sample(numbers, k=quantity) # Випадкові унікальні числа з діапазону
             return sorted(lottery_numbers) # Повертаємо відсортований список
